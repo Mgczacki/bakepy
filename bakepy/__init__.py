616cac52416f7959f036247a3e3c693d573ffc83
mario_gc <mario.garrido@kuona.co>
2022-10-02 21:38:43 -0500

    First commit

---
+++ bakepy/__init__.py
@@ -0,0 +1,6 @@
+"""BakePy Markdown package."""
+__version__ = '0.0.1'
+
+from .ipython import load_ipython_extension
+
+from .report import Report
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bakepy',
+    packages=find_packages(include=['bakepy']),
+    version='0.0.1',
+    description='BakePy Library.',
+    author='mg@czacki.com',
+    license='NoLicenseYet')
